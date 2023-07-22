5fc12dc18e55d80e1dfcac1716d140086d6de358
Swapnil Thatte <thatte.swapnil@gmail.com>
2023-07-22 21:58:21 +0530

    updates

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,11 +1,10 @@
 //NAVBAR
 window.onscroll = () => {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         document.getElementById("navbar").style.backgroundColor =
-            "rgba(10, 10, 10, 0.674)";
+            "rgba(10, 10, 10)";
+    } else {
+        document.getElementById("navbar").style.backgroundColor =
+            "rgb(19, 87, 19)";
     }
-    else {
-         document.getElementById("navbar").style.backgroundColor =
-             "rgba(106, 166, 255, 0.63)";
-    }
-}
+};
---
+++ script.js
@@ -0,0 +1,15 @@
+// const btn = document.getElementById("hamburger");
+
+// let click_count = 0
+
+// btn.addEventListener('click', event => {
+//     // console.log("Hello");
+//     const navbar = document.getElementById("navbar-right");
+//     if (click_count%2 != 0){
+//         navbar.style.display = "none";
+//     }
+//     else {
+//         navbar.style.display = "block";
+//     }
+//     click_count++;
+// })
